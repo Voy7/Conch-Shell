465f161f4638a95a79e46697c084d8a0cdfdd1f1
Voy7 <89422331+Voy7@users.noreply.github.com>
2023-07-18 01:33:57 -0500

    Built-in FFMPEG import

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import Logger from '#src/classes/Logger'
 import packageJSON from '#root/package.json' assert { type: 'json' }
 import '#src/classes/BotHandler'
 
+import { path } from '@ffmpeg-installer/ffmpeg'
+import ffmpeg from 'fluent-ffmpeg'
+ffmpeg.setFfmpegPath(path)
+
 // Clear console and print startup message
 console.clear()
 Logger.info(`Starting Conch-Shell v${packageJSON.version}...`)
